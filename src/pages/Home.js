8bfe423ac8d12cd2b08d06beb2f55019108748d1
PogoNguyen03 <105332052+PogoNguyen03@users.noreply.github.com>
2025-04-12 10:00:47 +0700

    delete

--- src/pages/Home.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div className="text-center text-white">
-      <h2 className="text-4xl font-bold">Welcome to MovieZone</h2>
-      <p className="text-gray-400 mt-2">Enjoy unlimited movies and TV shows.</p>
-    </div>
-  );
-}
