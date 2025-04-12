810280a5dabd95778aef5d6eaad0aad8d657f1aa
PogoNguyen03 <105332052+PogoNguyen03@users.noreply.github.com>
2025-04-12 10:03:19 +0700

    setup
    setup ramcms

---
+++ application/extra/version.php
@@ -0,0 +1,9 @@
+<?php
+return array (
+    'name' => 'RamCMS',
+    'copyright' => 'RamCMS',
+    'url' => '//github.com/brevis-ng/ramcms',
+    'code' => '2023.1000.3052.2',
+    'license' => 'MIT',
+);
+?>
