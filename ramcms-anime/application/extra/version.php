a137f4e92b20cbcb5b9f4a6fabe2c39fafa0491b
PogoNguyen03 <105332052+PogoNguyen03@users.noreply.github.com>
2025-04-22 11:21:08 +0700

    delete duplicate
    delete duplicate ramcms

--- ramcms-anime/application/extra/version.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array (
-    'name' => 'RamCMS',
-    'copyright' => 'RamCMS',
-    'url' => '//github.com/brevis-ng/ramcms',
-    'code' => '2023.1000.3052.2',
-    'license' => 'MIT',
-);
-?>
